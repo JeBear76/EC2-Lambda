9743da4a9d263a227cfd9086a247e236e173772b
JEBEAR-PC\jebear <jerepondumie@gmail.com>
2024-02-09 18:19:30 +0200

    removed Keypair creation

--- ProcessStarter/runProcess.sh
+++ ProcessStarter/runProcess.sh
@@ -1,9 +1,9 @@
-﻿#!/bin/sh
-dotnet ~/apps/processStarter/ProcessStarter.dll
+﻿#!/bin/bash
+(dotnet ~/apps/processStarter/ProcessStarter.dll
 
-aws s3api put-object --bucket ec2-lambda-output --key processStarter.log --body ~/processStarter.log
+aws s3api put-object --bucket jdt-ec2-lambda-output --key processStarter.log --body processStarter.log
 
 instanceId="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
 json="\"instance\":\"${instanceId}\", \"dryRun\":\"False\""
-aws lambda invoke --function-name arn:aws:lambda:eu-west-1:523759632228:function:ec2KillerLambda --invocation-type Event --payload "{`echo $json`}" --region eu-west-1 response.json
+aws lambda invoke --function-name arn:aws:lambda:eu-west-1:523759632228:function:jdt-ec2KillerLambda --invocation-type Event --payload "{`echo $json`}" --region eu-west-1 response.json) 2>&1
 
